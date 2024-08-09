8512a6e19b2c69ea892be6156db3ef32b0472c6a
fayoola <fayola_luvena_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 10:53:34 +0700

    Add files via upload

---
+++ Luas layang-layang.java
@@ -0,0 +1,11 @@
+public class Luas layang-layang {
+    System.out.println("Selamat datang di program luas kayang-layang");
+    double diagonal1,diagonal2,hasil;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan diagonal1 layang-layang: ");
+        diagonal1 = input.nextDouble();
+        System.out.println("Masukkan diagonal2: ");
+        diagonal2 = input.nextDouble();
+        hasil = diagonal1*diagonal2*1/2;
+        System.out.println("Hasil luas layang-layang adalah: "+hasil);
+}
